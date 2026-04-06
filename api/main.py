d4fc3e7af6cf5fafcf9bee76930261a5dd7cfa0a
Edisson A.G.C. <ingenieroedissonia@gmail.com>
2026-04-05 20:48:39 -0500

    fix: allow_origins especifico CORS - R094

--- api/main.py
+++ api/main.py
@@ -56,7 +56,7 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://maiie-systems.vercel.app"],
+    allow_origins=["http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "https://maiie-systems-graph.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
