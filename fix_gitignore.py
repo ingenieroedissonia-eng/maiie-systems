0dc4dd99575e4e4349512712407b7cc5dd24132b
Edisson A.G.C. <ingenieroedissonia@gmail.com>
2026-04-11 00:56:35 -0500

    chore: limpieza scripts temporales R105

--- fix_gitignore.py
+++
@@ -1,7 +0,0 @@
-﻿content = open('.gitignore', encoding='utf-8').read()
-if 'maiie-web/dist/' not in content:
-    content += '\nmaiie-web/dist/\n'
-    open('.gitignore', 'w', encoding='utf-8').write(content)
-    print('OK - dist agregado')
-else:
-    print('YA EXISTE')
